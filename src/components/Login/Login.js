f61dc3e448e4822dec2042c83c073a7fbaf2fa05
shahadat2772 <shahadat2772@gmailcom>
2022-04-10 11:11:27 +0600

    input form added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Login.css";
 
 const Login = () => {
   return (
-    <div>
-      <h3>This is login</h3>
+    <div className="form-container">
+      <div>
+        <p className="form-title">Login</p>
+        <form action="">
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" name="" id="" required />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" name="" id="" required />
+          </div>
+          <input type="submit" className="form-submit" value="Login" />
+        </form>
+        <p className="form-link-text">
+          New to Ema-john?{" "}
+          <Link to={`/signup`} className="form-link">
+            Create New Account
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,9 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const SIgnUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleEmailBlur = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePasswordBlur = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleConfirmPasswordBlur = (e) => {
+    setConfirmPassword(e.target.value);
+  };
+
   return (
-    <div>
-      <h3>Thsi is sign up</h3>
+    <div className="form-container">
+      <div>
+        <p className="form-title">Sign Up</p>
+        <form action="">
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              onBlur={handleEmailBlur}
+              type="email"
+              name=""
+              id=""
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              onBlur={handlePasswordBlur}
+              name=""
+              id=""
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Confirm Password</label>
+            <input
+              type="password"
+              onBlur={handleConfirmPasswordBlur}
+              name=""
+              id=""
+            />
+          </div>
+          <input type="submit" className="form-submit" value="Login" required />
+        </form>
+        <p className="form-link-text">
+          Already have an account?{" "}
+          <Link to={`/login`} className="form-link">
+            Login
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
